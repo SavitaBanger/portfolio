de15866d2ba020a69d31400250e01c4cdf6fef4c
Savita Banger <159711594+SavitaBanger@users.noreply.github.com>
2025-10-14 13:54:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,31 @@
-$(document).ready(function() {
 
-	
-	/* Navigation burger onclick side navigation show */
-	$('.burger-container').on('click', function() {
-		$('.main-navigation').toggle('slow');
+    let cursor = $(".cursor");
+    let follower = $(".cursor2");
 
-		if($('#myBtn').hasClass('change')) {
-			$('body').addClass('stop-scroll');
-		} else {
-			$('body').removeClass('stop-scroll');
-		}
-	});
+    let posX = 0,
+        posY = 0;
 
+    let mouseX = 0,
+        mouseY = 0;
 
-	/* About me slider */
-	$('.about-me-slider').slick({
-		slidesToShow: 1,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>'
-	});
+    gsap.ticker.add(() => {
+        posX += (mouseX - posX) / 9;
+        posY += (mouseY - posY) / 9;
 
-	/* Blog slider */
-	$('.blog-slider').slick({
-		slidesToShow: 2,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>',
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
-	
-});
+        gsap.set(follower, {
+            x: posX - 12,
+            y: posY - 12,
+        });
 
+        gsap.set(cursor, {
+            x: mouseX,
+            y: mouseY,
+        });
+    });
 
+    $(document).on("mousemove", (e) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    });
 
-var counta = 0;
-
-$(window).scroll(function(e){
-
-
-	/* Onscroll number counter */
-	var statisticNumbers = $('.single-count');
-	if(statisticNumbers.length) {
-		var oTop = statisticNumbers.offset().top - window.innerHeight;
-		if (counta == 0 && $(window).scrollTop() > oTop) {
-			$('.count').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-						$this.text(this.countNum);
-					}
-				});
-			});
-			counta = 1;
-		}
-	}
-
-});
+    
